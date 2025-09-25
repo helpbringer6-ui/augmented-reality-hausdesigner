238c5721754ecf63020f86f1bc1125ebd93be5bd
helpbringer6-ui <bdtwalalo@hotmail.com>
2025-09-25 09:32:25 -0500

    feat: initial commit for app.js

---
+++ app.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // TODO: Initialize AR functionality
+    console.log('AR Hausdesigner geladen');
+    // Hier sollte die Logik für die Echtzeit-Visualisierung und Möbelplatzierung folgen.
+});
